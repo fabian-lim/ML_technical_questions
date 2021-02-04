9374dd663588cf445ce5890ec1e613028eab38d4
fabian-lim <66361124+fabian-lim@users.noreply.github.com>
2021-02-05 00:29:59 +0800

    Update ml_tech_questions.js

--- ml_tech_questions.js
+++ ml_tech_questions.js
@@ -63,7 +63,7 @@ fibonaci_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
 // # Answer for question 2:
 const fibonaci = (n) => {
     if (n <= 1) {
-        return 1
+        return n
     } else {
         return fibonaci(n-1) + fibonaci(n-2)
     }
