e4884550b386eb8e86a493729b992650a654d695
fabian-lim <66361124+fabian-lim@users.noreply.github.com>
2021-02-05 00:32:43 +0800

    Update ML_tech_questions.py

--- ML_tech_questions.py
+++ ML_tech_questions.py
@@ -67,7 +67,7 @@ fibonaci_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
 
 def fibonaci(n):
     if n == 0:
-        return 1
+        return 0
     elif n == 1:
         return 1
     else:
